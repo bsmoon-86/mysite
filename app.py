fc2486b0a3f19ad069b02451c8d0e0374ea52575
bsmoon <45582617+bsmoon-86@users.noreply.github.com>
2025-03-31 16:34:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # 프레임워크 로드 
-from flask import Flask, request, render_template, url_for, redirect
+from flask import Flask, request, render_template, url_for, redirect, session
 import pandas as pd
 import invest
 from database import MyDB
@@ -12,6 +12,9 @@ load_dotenv()
 
 # Flask Class 생성 
 app = Flask(__name__)
+
+# session에 비밀키 지정 
+app.secret_key = os.getenv('secret')
 
 # database Class 생성 
 mydb = MyDB(
@@ -36,6 +39,23 @@ def signup():
     return render_template('signup.html')
 
 # 로그인 api
+@app.route('/signin', methods=['post'])
+def signin():
+    input_id = request.form['id']
+    input_pass = request.form['password']
+
+    login_result = mydb.execute_query(
+        querys.login_query, 
+        input_id, 
+        input_pass
+    )
+    # login_result의 길이가 1이면 로그인 성공 
+    if len(login_result) == 1:
+        # 로그인 성공시 세션에 데이터 저장
+        session['user_info'] = [input_id, input_pass]
+        return redirect('/invest')
+    else:
+        return redirect('/')
 
 # 회원 가입 api 
 @app.route('/signup2', methods=['post'])
@@ -63,17 +83,16 @@ def signup2():
 # 페이지를 보여주는 api 생성
 @app.route('/invest')
 def first():
-    return render_template('invest.html')
+    # session에 데이터가 존재하는가?
+    if 'user_info' in session:
+        return render_template('invest.html')
+    else:
+        return redirect('/')
 
-
-# 대쉬보드 페이지를 보여주는 api 생성 
-# 대쉬보드에서 필요한 데이터는 
-# table_cols (표에서 컬럼의 이름들) list
-# table_data (표에서 벨류의 값들) dict
-# x_data (챠트에서 x축 데이터) list
-# y_data (탸츠에서 y축 데이터) list
 @app.route('/dashboard')
 def dashboard():
+    if 'user_info' not in session:
+        return redirect('/')
     # 유저가 보낸 데이터를 변수에 저장 
     # get 방식으로 보내온 데이터는 request.args에 존재 
     # 종목코드 
