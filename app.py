8f47376dca5b0d2924aa4d39bbf17c4477e00fcc
bsmoon <45582617+bsmoon-86@users.noreply.github.com>
2025-06-25 00:12:52 +0900

    update

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 # 프레임워크 로드 
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 import pandas as pd
-from invest import Quant, load_data
+from invest import Quant
+import database
 
-
+mydb = database.MyDB()
 # Flask class 생성 
 # 생성자 함수 필요한 인자 : 파일의 이름 
 app = Flask(__name__)
@@ -14,11 +15,32 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    return render_template('test.html')
+    return render_template('login.html')
 
 @app.route('/main', methods=['post'])
 def main():
-    return render_template('index.html')
+    _id = request.form['input_id']
+    _pass = request.form['input_pass']
+    login_query = """
+        select 
+        * 
+        from 
+        user 
+        where 
+        id = %s 
+        and 
+        password = %s
+    """
+
+    # 함수 호출
+    db_result = mydb.sql_query(login_query, _id, _pass)
+    # 로그인의 성공 여부 (조건식?? db_result가 존재하는가?)
+    if db_result:
+        return render_template('index.html')
+    else:
+        # 로그인이 실패하는 경우 -> 로그인화면('/')으로 되돌아간다.
+        return redirect('/')
+        # return "login fail"
 
 @app.route('/invest')
 def invest():
