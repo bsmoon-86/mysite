7062d7a8996927543514fb8e334cdae9d30f3d2d
moons <ekflafg@gmail.com>
2025-06-25 10:22:11 +0900

    udpdate

--- db_setting.py
+++ db_setting.py
@@ -1,15 +1,8 @@
 import pymysql
+from database import MyDB
 
-_db = pymysql.connect(
-    host = 'moons9303.mysql.pythonanywhere-service.com', 
-    port = 3306, 
-    user = 'moons9303', 
-    password = 'moon1234', 
-    db = 'moons9303$ubion'
-)
-
-# cursor 생성
-cursor = _db.cursor(pymysql.cursors.DictCursor)
+# MyDB class 생성 
+mydb = MyDB()
 
 # table 생성 쿼리문 
 create_user = """
@@ -21,11 +14,19 @@ create_user = """
     name varchar(32) 
     )
 """
+# sql 쿼리문을 실행
+mydb.sql_query(create_user)
+# db server에 동기화하고 연결을 종료 
+mydb.commit_db()
 
-# sql 쿼리문을 실행 
-cursor.execute(create_user)
-# 동기화
-_db.commit()
-# 서버와의 연결을 종료
-_db.close()
-print('table 생성 완료')
+
+
+
+
+# _db = pymysql.connect(
+#     host = 'moons9303.mysql.pythonanywhere-service.com', 
+#     port = 3306, 
+#     user = 'moons9303', 
+#     password = 'moon1234', 
+#     db = 'moons9303$ubion'
+# )
